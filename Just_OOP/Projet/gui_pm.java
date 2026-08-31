7ede059073fcd4f6ff6765810863409b76e4b183
SetD <mynamepanpan@gmail.com>
2026-08-31 12:54:47 +0700

    Clean code next to Upd

--- Just_OOP/Projet/gui_pm.java
+++ Just_OOP/Projet/gui_pm.java
@@ -35,6 +35,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
     private String stateInput; //ตัวแปลส่งข้อมูลสถานะในการทำงานของผู้ใช้
     public int Wginter = 1024,Hginter = 1024;
     private int perLowD; //ตัวแปลเลขที่เอาไว้คำนวณ ของฝนเทียม
+    int Tcol = 40,Trow=20;
     gui_pm() {
         //==================================
         // เริ่ม GUI เค้าโครง
@@ -171,7 +172,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
              int numData;
             int r = table.getSelectedRow();
             int c = table.getSelectedColumn();
-            int cellNumber = (r * 40) + c + 1;
+            int cellNumber = (r * Tcol) + c + 1;
             String Output = String.format("User On cell %d or on Row:: %d and Col:: %d",cellNumber,r,c);
             System.out.println(Output);
             if(!((r != -1) && (c != -1))){
@@ -220,7 +221,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
         else if(e.getActionCommand().equals("RAIN")){
             int r = table.getSelectedRow();
             int c = table.getSelectedColumn();
-            int cellNumber = (r * 40) + c + 1;
+            int cellNumber = (r * Tcol) + c + 1;
             String Output = String.format("User On cell %d or on Row:: %d and Col:: %d",cellNumber,r,c);
             System.out.println(Output); 
             if(Input_crete3.getJList().getSelectedIndex()==0){
@@ -271,7 +272,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
     // Method ชุดสร้างตาราง
     private JTable setup_Tables() {
         int numRows = 1;
-        table = new JTable(20, 40 ){
+        table = new JTable(Trow, Tcol ){
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false; 
@@ -289,7 +290,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
             }
         };
         
-        table.setRowHeight(40); 
+        table.setRowHeight(Tcol); 
         table.setShowGrid(true);
         table.setGridColor(Color.BLACK); 
         table.setIntercellSpacing(new Dimension(1, 1));
@@ -379,7 +380,7 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
 
     //Method สำหรับในการแสดงตำแหน่งที่ผู้ใช้คลิกอยู่ว่าที่ช่องเท่าไหร่
     private void onCellSelected(int row, int col,jpaleBox Box) {
-        int cellNumber = (row * 40) + col + 1;
+        int cellNumber = (row * Tcol) + col + 1;
         Box.setValue(Integer.toString(cellNumber));
 
     }
@@ -582,9 +583,9 @@ public class gui_pm extends JFrame implements ActionListener,MouseListener{
     private void Artificial_Rain(int range,int row,int col){
         int offset = (int)range/2;
         int minRow = Math.max(0, row - offset);
-        int maxRow = Math.min(20 - 1, row + offset);
+        int maxRow = Math.min(Trow - 1, row + offset);
         int minCol = Math.max(0, col - offset);
-        int maxCol = Math.min(40 - 1, col + offset);
+        int maxCol = Math.min(Tcol - 1, col + offset);
         SetDustForArtificial(maxRow,minRow,maxCol,minCol);
 
     }
